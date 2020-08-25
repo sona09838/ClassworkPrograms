8e6656eb3bae8c3497d7dd2374370fc4d706e74b
sonali <ssonalishinde1995.sgcs@gmail.com>
2020-08-25 13:41:42 +0530

    3 repository

---
+++ Classwork/src/com/assignment/methods/AccountManagement.java
@@ -0,0 +1,31 @@
+package com.assignment.methods;
+
+public class AccountManagement {
+	
+	void insertdata()
+	{
+		System.out.println("Account details of customer");
+	}
+	void displayData()
+	{
+		System.out.println("Account details of customer");
+	}
+	void depositeAmount()
+	{
+		System.out.println("Deposite amount");
+	}
+	void withdrawAmount()
+	{
+		System.out.println("Withdraw amount");
+	}
+	public static void main(String[] args) {
+		AccountManagement a=new AccountManagement();
+		a.insertdata();
+		a.displayData();
+		a.depositeAmount();
+		a.withdrawAmount();
+		// TODO Auto-generated method stub
+
+	}
+
+}
